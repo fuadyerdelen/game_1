cca463d85961164ddb6d1fa3fb17ac6e034d4e61
fuad <m.fuadyerdelen@gmail.com>
2021-04-16 19:31:19 +0200

    make rasponsive

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-function name(params) {
-    
+
+
+
+
+
+    function getColorName(color) {
+
+        let color1 = color;
+        let color2 = color;
+    }
+
+    let color1
+
+    let color2
+
+
+
+
+
+
+    function gameOn(){
+
+        if (color1 === color2){
+
+            document.getElementById("").style.display= "none";
+            document.getElementById("").style.display= "none";
+        }
+
+
 }
+
+    
+
